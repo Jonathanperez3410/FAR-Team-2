8a52cc717ee456b222703cf91ca00f4105804410
Joshua Kopp <kopp.josh415@gmail.com>
2022-05-28 15:12:27 -0400

    Changed project from VSCode to CLion

--- Avionics/HadesAvionics/src/main.cpp
+++ Avionics/HadesAvionics/src/main.cpp
@@ -5,10 +5,11 @@
 #include <SD.h>
 #include <SPI.h>
 
-#define Lora Serial2
+const int BUZZER = 2;
 
-#define GPSSerial Serial3
-Adafruit_GPS GPS(&GPSSerial);
+// #define GPSSerial Serial3
+// Adafruit_GPS GPS(&GPSSerial);
+Adafruit_GPS GPS(&Serial3);
 
 Adafruit_BMP280 BMP;
 
@@ -20,113 +21,233 @@ int isBMPActive, isBNOActive, isGPSActive, isLoraActive, isSDActive;
 
 uint8_t sysCal, gyroCal, accelCal, magCal;
 
-uint32_t timer = millis();
+// uint32_t timer = millis();
+
+const int DEBUG_MODE = 1;
 
 File dataOut;
 
-void getTime()
-{
-	// GPS.read();
-	// if (GPS.newNMEAreceived())
-	// 	if (!GPS.parse(GPS.lastNMEA()))
-	// 		return;
+void sdInit();
+void gpsInit();
+void bmpInit();
+void bnoInit();
 
-	Serial.print(GPS.year, DEC); Serial.print("-");
-	Serial.print(GPS.month, DEC); Serial.print("-");
-	Serial.print(GPS.day, DEC); Serial.print("-");
-	Serial.println("");
-}
+void writeBMPData();
+void writeBNOData();
 
-void writeBMPData()
-{
-	dataOut.print((String)BMP.readTemperature() + ",");
-	dataOut.print((String)BMP.readPressure() + ",");
-}
-
-void writeBNOData()
-{
-	sysCal = gyroCal = accelCal = magCal = 0;
-	BNO.getCalibration(&sysCal, &gyroCal, &accelCal, &magCal);
-
-	imu::Vector<3> euler = BNO.getVector(Adafruit_BNO055::VECTOR_EULER);
-	imu::Vector<3> gyro = BNO.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-	imu::Vector<3> accel = BNO.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-
-	if (sysCal == 3)
-	{
-		dataOut.print((String)euler.x() + ",");
-		dataOut.print((String)euler.y() + ",");
-		dataOut.print((String)euler.z() + ",");
-
-		dataOut.print((String)gyro.x() + ",");
-		dataOut.print((String)gyro.y() + ",");
-		dataOut.print((String)gyro.z() + ",");
-
-		dataOut.print((String)accel.x() + ",");
-		dataOut.print((String)accel.y() + ",");
-		dataOut.print((String)accel.z() + ",");
-	}
-	else
-	{
-		for (int i = 0; i < 9; i++)
-			dataOut.print("NO_BNO_CAL,");
-	}
-
-}
+void getTime();
+void happyBeep();
+void sadBeep();
+void numBeeps(int beeps);
 
 void setup()
 {
-	Serial.begin(115200);
-	
-	// Initialize the BMP280
-	if (!BMP.begin())
-		isBMPActive = 0;
-	else
-		isBMPActive = 1;
+    if (DEBUG_MODE)
+        Serial.begin(115200);
 
-	BMP.setSampling(Adafruit_BMP280::MODE_NORMAL,
-					Adafruit_BMP280::SAMPLING_X2,
-					Adafruit_BMP280::SAMPLING_X16,
-					Adafruit_BMP280::FILTER_X16,
-					Adafruit_BMP280::STANDBY_MS_500);
+    pinMode(BUZZER, OUTPUT);
 
-
-	// Initialize the BNO055
-	if (!BNO.begin())
-		isBNOActive = 0;
-	else
-		isBNOActive = 1;
-
-	BNO.setExtCrystalUse(true);
-
-
-	// Initialize the GPS
-	GPS.begin(9600);
-	GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-	GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-	GPS.sendCommand(PGCMD_ANTENNA);
-
-	isGPSActive = 1;
-
-
-	// Initialize the SD Card
-	if (!SD.begin(BUILTIN_SDCARD))
-		isSDActive = 0;
-	else
-	{
-		isSDActive = 1;
-		dataOut = SD.open("data.csv", FILE_WRITE);
-	}
+    // sdInit();
+    gpsInit();
+    // bmpInit();
+    // bnoInit();
 }
 
 void loop()
 {
-	dataOut = SD.open("data.fuck", FILE_WRITE);
+    // char c = GPS.read();
+    GPS.read();
 
-	writeBNOData();
-	writeBMPData();
+    if (GPS.newNMEAreceived())
+    {
+        Serial.print(GPS.lastNMEA());
+        if (!GPS.parse(GPS.lastNMEA()))
+            return;
+    }
 
-	dataOut.println();
-	dataOut.close();
-	delay(500);
+    if (GPS.fix)
+    {
+        Serial.println("FIX");
+        delay(100);
+    }
+
+}
+
+void sdInit()
+{
+    numBeeps(1);
+    delay(500);
+
+    if (!SD.begin(BUILTIN_SDCARD))
+    {
+        isSDActive = 0;
+        sadBeep();
+    }
+    else
+    {
+        isSDActive = 1;
+        happyBeep();
+    }
+
+    delay(1000);
+}
+
+void gpsInit()
+{
+    numBeeps(2);
+    delay(500);
+
+    GPS.begin(9600);
+    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+    GPS.sendCommand(PGCMD_ANTENNA);
+
+    isGPSActive = 1;
+    happyBeep();
+
+    delay(1000);
+
+    /*
+    while (1)
+    {
+        GPS.read();
+        if (GPS.newNMEAreceived())
+        {
+            Serial.print(GPS.lastNMEA());
+            GPS.parse(GPS.lastNMEA());
+        }
+
+        if (GPS.fix)
+            return;
+
+        Serial.println("NO FIX");
+        delay(500);
+    }
+    */
+}
+
+void bmpInit()
+{
+    numBeeps(3);
+    delay(500);
+
+    if (!BMP.begin())
+    {
+        isBMPActive = 0;
+        sadBeep();
+    }
+    else
+    {
+        isBMPActive = 1;
+        happyBeep();
+    }
+
+    BMP.setSampling(Adafruit_BMP280::MODE_NORMAL,
+                    Adafruit_BMP280::SAMPLING_X2,
+                    Adafruit_BMP280::SAMPLING_X16,
+                    Adafruit_BMP280::FILTER_X16,
+                    Adafruit_BMP280::STANDBY_MS_500);
+
+    delay(1000);
+}
+
+void bnoInit()
+{
+    numBeeps(4);
+    delay(500);
+
+    if (!BNO.begin())
+    {
+        isBNOActive = 0;
+        sadBeep();
+    }
+    else
+    {
+        isBNOActive = 1;
+        happyBeep();
+    }
+
+    BNO.setExtCrystalUse(true);
+
+    delay(1000);
+}
+
+void writeBMPData()
+{
+    dataOut.print((String)BMP.readTemperature() + ",");
+    dataOut.print((String)BMP.readPressure() + ",");
+}
+
+void writeBNOData()
+{
+    sysCal = gyroCal = accelCal = magCal = 0;
+    BNO.getCalibration(&sysCal, &gyroCal, &accelCal, &magCal);
+
+    imu::Vector<3> euler = BNO.getVector(Adafruit_BNO055::VECTOR_EULER);
+    imu::Vector<3> gyro = BNO.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+    imu::Vector<3> accel = BNO.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+
+    if (sysCal == 3)
+    {
+        dataOut.print((String)euler.x() + ",");
+        dataOut.print((String)euler.y() + ",");
+        dataOut.print((String)euler.z() + ",");
+
+        dataOut.print((String)gyro.x() + ",");
+        dataOut.print((String)gyro.y() + ",");
+        dataOut.print((String)gyro.z() + ",");
+
+        dataOut.print((String)accel.x() + ",");
+        dataOut.print((String)accel.y() + ",");
+        dataOut.print((String)accel.z() + ",");
+    }
+    else
+    {
+        for (int i = 0; i < 9; i++)
+            dataOut.print("NO_BNO_CAL,");
+    }
+}
+
+void getTime()
+{
+    // GPS.read();
+    // if (GPS.newNMEAreceived())
+    // 	if (!GPS.parse(GPS.lastNMEA()))
+    // 		return;
+
+    Serial.print(GPS.year, DEC); Serial.print("-");
+    Serial.print(GPS.month, DEC); Serial.print("-");
+    Serial.print(GPS.day, DEC); Serial.print("-");
+    Serial.println("");
+}
+
+void happyBeep()
+{
+    tone(BUZZER, 1568);
+    delay(250);
+    tone(BUZZER, 2093);
+    delay(250);
+    noTone(BUZZER);
+}
+
+void sadBeep()
+{
+    tone(BUZZER, 1480);
+    delay(250);
+    noTone(BUZZER);
+    delay(50);
+    tone(BUZZER, 1480);
+    delay(250);
+    noTone(BUZZER);
+}
+
+void numBeeps(int beeps)
+{
+    for (int i = 0; i < beeps; i++)
+    {
+        tone(BUZZER, 1800);
+        delay(250);
+        noTone(BUZZER);
+        delay(250);
+    }
 }
