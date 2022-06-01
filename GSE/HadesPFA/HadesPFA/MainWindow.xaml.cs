0ce93dbe75ebf6456359a885dcfc1376a3219b99
Joshua Kopp <kopp.josh415@gmail.com>
2022-06-01 01:07:55 -0400

    Tested file import system for flight data

--- GSE/HadesPFA/HadesPFA/MainWindow.xaml.cs
+++ GSE/HadesPFA/HadesPFA/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace HadesPFA
 {
@@ -23,6 +13,17 @@ namespace HadesPFA
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		void btnOpenFile_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog
+			{
+				Filter = "KXR Flight Data (*.kxr)|*.kxr|All files (*.*)|*.*"
+			};
+
+			if (openFileDialog.ShowDialog() == true)
+				txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
 		}
 	}
 }
